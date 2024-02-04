5d1a91f68ef836684d823abc45aebdf0e5b2dcc1
Uzair <uzairarifjokhio@gmail.com>
2024-02-04 17:03:23 +0500

    learning API jwt, djoser, token based authentiction

--- chefsTable/settings.py
+++ chefsTable/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES' : [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_xml.renderers.XMLRenderer',
+        # 'rest_framework_xml.renderers.XMLRenderer',
     ],
 
     'DEFAULT_FILTER_BACKENDS': [
@@ -140,6 +140,4 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE' : 3,
-
-    
 }
